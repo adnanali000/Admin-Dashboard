957be4b0ff465cd2d99745eec3520f1770f35e55
adnanali000 <adnan.museel123@gmail.com>
2022-06-01 00:09:05 +0500

    pages route work done

---
+++ src/pages/list/List.jsx
@@ -0,0 +1,11 @@
+import './list.scss'
+
+const List = () => {
+  return (
+    <div className="home">
+       List
+    </div>
+  )
+}
+
+export default List
---
+++ src/pages/new/New.jsx
@@ -0,0 +1,11 @@
+import './new.scss'
+
+const New = () => {
+  return (
+    <div className="home">
+       New
+    </div>
+  )
+}
+
+export default New
